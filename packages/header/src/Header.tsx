01bc1de12d15f68d6e2a4e93ac498bbb8e0ac817
alex-loktiev <alexander.loktiev@techmagic.co>
2022-03-14 00:38:00 +0200

    Init

--- packages/header/src/Header.tsx
+++ packages/header/src/Header.tsx
@@ -4,10 +4,12 @@ type Props = {
     text: string
 }
 
-export const Header: FC<Props> = ({text}) => {
+const Header: FC<Props> = ({text}) => {
     return (
         <header>
             {text}
         </header>
     );
 };
+
+export default Header
