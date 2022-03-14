a95649789fa77c7a800516ce643ad7d4ef36aefb
alex-loktiev <alexander.loktiev@techmagic.co>
2022-03-14 20:41:16 +0200

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Header} from '@axeloktiev/header';
 
 function App() {
   return (
@@ -18,6 +19,8 @@ function App() {
         >
           Learn React
         </a>
+
+        <Header/>
       </header>
     </div>
   );
